a21f099b56d559527a57ac6b3a1053675869877b
willowjordan <willowtjordan@gmail.com>
2025-04-23 21:19:21 -0400

    added card images

--- card.py
+++ card.py
@@ -1,5 +1,6 @@
 """This script defines the Card class, which represents a single playing card."""
 
+from tkinter import PhotoImage
 from enum import Enum
 
 class Suit(Enum):
@@ -9,6 +10,7 @@ class Suit(Enum):
     SPADES = 3
 
 class Card():
+    PLAYING_CARD_PATH = "./sprites/card_{suit}_{value}.png"
     def __init__(self, suit:Suit, value:int):
         """
         :param suit: Suit of the card.
@@ -17,3 +19,18 @@ class Card():
         if (value < 1) | (value > 13): raise ValueError(f"Cannot create a card with value {value}")
         self.suit = suit
         self.value = value
+
+        # get photoimage object
+        if value == 1:
+            value_str = "A"
+        elif value < 10:
+            value_str = f"0{value}"
+        elif value == 11:
+            value_str = "J"
+        elif value == 12:
+            value_str = "Q"
+        elif value == 13:
+            value_str = "K"
+        suit_str = suit.name.lower()
+        path = Card.PLAYING_CARD_PATH.format(suit=suit_str, value=value_str)
+        self.image = PhotoImage(file=path)
